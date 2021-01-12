5e9b6f37883f4e4aef5745d733ed040bffd9aaf9
Jo <joannadawnmiller@gmail.com>
2021-01-12 13:42:28 -0500

    add more imgs with toggle effect

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+
+  $(".click").click(function() {
+    $("#hide-img").toggle();
+    $("#show-img").toggle();
+  });
 });
